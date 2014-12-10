8eadfd1ffc03ff1e38b52a3cd2f1ce4f73c51ea8
Haruki Irei <e135728@ie.u-ryukyu.ac.jp>
2014-12-10 11:39:54 +0900

    少し編集,途中(画像の出力)

--- TapBreak/TapBreak/GameScene.swift
+++ TapBreak/TapBreak/GameScene.swift
@@ -9,17 +9,24 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
+    /*override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "メインのゲームシーンです.";
         myLabel.fontSize = 30;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
         self.addChild(myLabel)
+    }*/
+    override init(size: CGSize){
+        super.init(size: size)
+        let sprite = SKSpriteNode(imageNamed:"Spaceship")
+        sprite.position = CGPoint(x:0,y:0)
+        
     }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    required init(coder aDecoder: NSCoder){
+        fatalError("NSCoder not supported")
+    }
+    /*override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
@@ -31,13 +38,13 @@ class GameScene: SKScene {
             sprite.yScale = 0.5
             sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            //let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            //sprite.runAction(SKAction.repeatActionForever(action))
             
             self.addChild(sprite)
         }
-    }
+    }*/
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
