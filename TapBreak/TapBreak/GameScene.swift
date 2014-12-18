83d552b50a11c3100092bcce102ce51812292195
Haruki Irei <e135728@ie.u-ryukyu.ac.jp>
2014-12-18 12:28:01 +0900

    10回タップすると次の画像を出力するコード追加

--- TapBreak/TapBreak/GameScene.swift
+++ TapBreak/TapBreak/GameScene.swift
@@ -14,40 +14,46 @@ class GameScene: SKScene {
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
     }
-    /*override init(size: CGSize) {
-        maru = SKSpriteNode(imageNamed: "Black")
-        maru.alpha = 1
-        maru.position = CGPoint(x: 50, y: 50)
-        maru.size = CGSize(width: 100, height: 100)
-        super.init(size: size)
-        backgroundColor = SKColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
-        addChild(maru)
-    }*/
     override init(size: CGSize) {
-        
         maru = SKSpriteNode(imageNamed: "Black")
-        maru.alpha = 1
+        maru.alpha = 0.2
         maru.position = CGPoint(x: 100, y: 100)
-        //maru.size = CGSize(width: 100, height: 100)
-        //maru.xScale = 0.2
-        //maru.yScale = 0.2
         maru.name = "Black"
+        //maru.size = CGSize(width: 100, height: 100)
         super.init(size: size)
         backgroundColor = SKColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
         addChild(maru)
     }
+    /*func addBlack(size: CGSize) ->CGSize{
+        
+        maru = SKSpriteNode(imageNamed: "Black")
+        maru.alpha = 0.5
+        maru.position = CGPoint(x: 100, y: 100)
+        maru.name = "Black"
+        //super.init(size: size)
+        backgroundColor = SKColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
+        addChild(maru)
+        return size
+    }*/
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        //var out : SKSpriteNode
+        //var out: SKSpriteNode =  addBlack()
         let touch: AnyObject = touches.anyObject()!
         let location = touch.locationInNode(maru)
         let touchedNode = self.nodeAtPoint(location)
-        
-        //if (touchedNode.name != nil) {
-            if(touchedNode.name == "Black"){
-                counter += 1
-                println("touched Black \(counter)")
-            }
-            
-        //}
+        if(touchedNode.name == "Black"){
+            counter += 1
+            println("touched Black \(counter)")
+        }
+        if(counter == 10){
+            //addBlack(maru)
+            maru = SKSpriteNode(imageNamed: "Black")
+            maru.alpha = 0.2
+            maru.position = CGPoint(x: 100, y: 100)
+            maru.name = "Black"
+            //backgroundColor = SKColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
+            addChild(maru)
+        }
     }
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
